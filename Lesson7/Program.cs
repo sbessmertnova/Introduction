6dbab35dcef21c496d1e88f2fd5008e1d8e13f30
sbessmertnova <minnij@yandex.ru>
2022-02-21 20:56:22 +0300

    homework

---
+++ Lesson7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secret = "123456";
+            Console.WriteLine("Введите пароль");
+            string input = Console.ReadLine();
+            if (input == secret)
+            {
+                Console.WriteLine("Пароль верный");
+            }
+
+        }
+    }
+}
