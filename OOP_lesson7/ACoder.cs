2a0078ed763ec28c05aed4653668d66f0ec9d5c0
sbessmertnova <minnij@yandex.ru>
2022-04-08 00:11:33 +0300

    OOP_Homework_Lesson7

---
+++ OOP_lesson7/ACoder.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace OOP_lesson7
+{
+    public class ACoder : ICoder
+    {
+        public string Decode(string encodedText)
+        {
+            return string.Concat(encodedText.Select(letter => (char)(letter - 1)));
+        }
+
+        public string Encode(string text)
+        {
+            return string.Concat(text.Select(letter => (char)(letter + 1)));
+        }
+    }
+}
