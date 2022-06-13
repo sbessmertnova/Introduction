9ba1ac053e5c30cf5a382280337c5bfc9f2aa706
sbessmertnova <minnij@yandex.ru>
2022-06-13 13:16:11 +0300

    lesson4

---
+++ Algorithms_lesson1/lesson4/Node.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_lesson1.lesson4
+{
+    public enum ParentLinkType
+    {
+        IsLeftChild,
+        IsRightChild
+    }
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> LeftNode { get; set; }
+        public Node<T> RightNode { get; set; }
+        public Node<T> Parent { get; set; }
+        public ParentLinkType? GetParentLinkType
+        {
+            get
+            {
+                if (this.Parent.LeftNode == null)
+                {
+                    return null;
+                }
+                if (this.Parent.LeftNode == this)
+                {
+                    return ParentLinkType.IsLeftChild;
+                }
+                else
+                {
+                    return ParentLinkType.IsRightChild;
+                }
+            }
+        }
+
+        public void PreOrderTravers(Node<T> root)
+        {
+            if (root != null)
+            {
+                Console.WriteLine($" {root.Value}");
+                PreOrderTravers(root.LeftNode);
+                PreOrderTravers(root.RightNode);
+            }
+        }
+        public void AddNode(Node<int> tree, int value)
+        {
+            if (SearchNode(tree, value)!=null)
+            {
+                Console.WriteLine("This node already exist");
+            }
+            else
+            {
+                var root = SearchRoot(tree);
+                SearchSpace(root.Value, tree);
+            }
+        }
+        public static void SearchSpace(int rootValue, Node<int> tree)
+        {
+            if (tree.Value>rootValue)
+            {
+                if (tree.RightNode == null)
+                {
+                    var newNode = new Node<int> { Value = tree.Value };
+                    tree.Parent.RightNode = newNode;
+                }
+                SearchSpace(tree.Value, tree.RightNode);
+            }
+            else
+            {
+                if (tree.LeftNode == null)
+                {
+                    var newNode = new Node<int> { Value = tree.Value };
+                    tree.Parent.LeftNode = newNode;
+                }
+                SearchSpace(tree.Value, tree.LeftNode);
+            }
+        }
+        public static Node<int> SearchRoot(Node<int> tree)
+        {
+            if (tree.Parent == null)
+            {
+                return tree;
+            }
+            else
+            {
+                return SearchRoot(tree.Parent);
+            }      
+        }
+        public void RemoveNode(Node<int> tree, int value)
+        {
+            var tempNode = SearchNode(tree, value);
+            if (IsTail(tempNode))
+            {
+                RemoveTailNode(tempNode);
+            }
+            else if (OnlyOneChild(tempNode))
+            {
+                var nodeToReplace = OnlyLeftChild(tempNode) ? tempNode.LeftNode : tempNode.RightNode;
+
+                if (tempNode.Parent.RightNode == tempNode)
+                {
+                    tempNode.Parent.RightNode = nodeToReplace;
+                }
+                else
+                {
+                    tempNode.Parent.LeftNode = nodeToReplace;
+                }
+            }
+            else
+            {
+                var minNode = SearchMinNode(tempNode.RightNode);
+                if (tempNode.GetParentLinkType == ParentLinkType.IsLeftChild)
+                {
+                    tempNode.Parent.LeftNode = minNode;
+                }
+                else
+                {
+                    tempNode.Parent.RightNode = minNode;
+                }
+            }
+        }
+        public static Node<int> SearchMinNode(Node<int> tree)
+        {
+            if (tree.LeftNode==null)
+            {
+                return tree;
+            }
+            else
+            {
+                return SearchMinNode(tree);
+            }
+        }
+        public static Node<int> SearchNode(Node<int> tree, int value)
+        {
+            switch (value.CompareTo(tree.Value))
+            {
+                case 0: return tree;
+                case 1: return SearchNode(tree.LeftNode, value);
+                case -1: return SearchNode(tree.RightNode, value);
+                default: return null;
+            }
+        }
+        public static void RemoveTailNode(Node<int>tree)
+        {
+            if (tree.Parent.LeftNode == tree)
+            {
+                tree.Parent.LeftNode = null;
+            }
+            else
+            {
+                tree.Parent.RightNode = null;
+            }
+        }
+        public static bool IsTail(Node<int> tree)
+        {
+            return tree.RightNode == null&& tree.LeftNode == null;
+        }
+        public static bool OnlyLeftChild(Node<int> tree)
+        {
+            return tree.LeftNode != null && tree.RightNode == null;
+        }
+        public static bool OnlyRightChild(Node<int> tree)
+        {
+            return tree.LeftNode == null && tree.RightNode != null;
+        }
+        public static bool OnlyOneChild(Node<int> tree)
+        {
+            return OnlyLeftChild(tree) || OnlyRightChild(tree);
+        }
+    }
+}
---
+++ Algorithms_lesson1/lesson4/NodeTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_lesson1.lesson4
+{
+   public class NodeTest
+    {
+        private const int newNodeValue = 15;
+        private const int valueForSearch = 6;
+        public static void TestNode()
+        {
+            var root = new Node<int>
+            {
+                Value = 8
+            };
+            var node10 = new Node<int>
+            {
+                Value = 10
+            };
+            var node14 = new Node<int>
+            {
+                Value = 14
+            };
+            var node3 = new Node<int>
+            {
+                Value = 3
+            };
+            var node1 = new Node<int>
+            {
+                Value = 1
+            };
+            var node6 = new Node<int>
+            {
+                Value = 6
+            };
+            var node4 = new Node<int>
+            {
+                Value = 4
+            };
+            var node7 = new Node<int>
+            {
+                Value = 7
+            };
+
+            root.RightNode = node10;
+            node10.RightNode = node14;
+            root.LeftNode = node3;
+            node3.LeftNode = node1;
+            node3.RightNode = node6;
+            node6.LeftNode = node4;
+            node6.RightNode = node7;
+
+            root.PreOrderTravers(root);
+           
+            root.AddNode(root, newNodeValue);
+            Console.WriteLine($"Проверка метода добавления. Дерево после добавления нового узла со значением {newNodeValue}");
+            root.PreOrderTravers(root);
+            
+            root.RemoveNode(root, newNodeValue);
+            Console.WriteLine($"Проверка метода удаления. Дерево после удаления хвостового узла со значением {newNodeValue}");
+            root.PreOrderTravers(root);
+            
+            root.RemoveNode(root, node4.Value);
+            Console.WriteLine($"Проверка метода удаления. Дерево после удаления узла из середины дерева со значением {node4.Value}");
+            root.PreOrderTravers(root);
+            
+            root.AddNode(root, node4.Value);
+            Console.WriteLine($"Возвращаем удаленный узел со значением {node4.Value}. " +
+                $"Дерево после возвращения узла:");
+            root.PreOrderTravers(root);
+
+            var foundNode = Algorithms_lesson1.lesson4.Node<int>.SearchNode(root, valueForSearch);
+            Console.WriteLine($"Проверка метода поиска. Найденный узел со значением {valueForSearch}:" +
+                $"\nValue: {foundNode.Value}" +
+                $"\nLeft node value: {foundNode.LeftNode.Value}" +
+                $"\nRight node value: {foundNode.RightNode.Value})");
+        }
+    }
+}
